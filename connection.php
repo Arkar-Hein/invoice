f4b568cc394b1a7207912495edbcae0581e5594e
Arkar-Hein <60578548+Arkar-Hein@users.noreply.github.com>
2020-02-24 22:04:05 +0630

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,15 +1,14 @@
 <?php
-$lhost="localhost";
-$user="root";
-$pswd="";
-$dbname="electron";
-$conn=mysqli_connect($lhost,$user,$pswd,$dbname);
-	if (!$conn)
-	{
-		echo "Failed to connect:".mysqli_connect_error();
-	}
-	else
-	{
-		echo "Success Connection";
-	}
+useMorrislaptop\Firestore\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+$serviceAccount =ServiceAccount::fromJsonFile(_DIR_'');
+$firestore=(new Factory)
+ ->withServiceAccount($serviceAccount)
+ ->createFirestore();
+ $collection =$firestore->collection('users');
+ $users =$collection->document('123456');
+ $user->set(['name'=>'morrislaptop','role'=>'developer']);
+ $snap=$user->snapshot();
+ echo $snap['name'];
 ?>
