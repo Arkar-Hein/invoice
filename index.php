b99080bffc316269930447d54b31c97cf3036e34
Arkar-Hein <60578548+Arkar-Hein@users.noreply.github.com>
2020-02-24 21:57:14 +0630

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <!DOCTYPE html>
 <html>
+<?php
+useMorrislaptop\Firestore\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+$serviceAccount =ServiceAccount::fromJsonFile(_DIR_'');
+$firestore=(new Factory)
+ ->withServiceAccount($serviceAccount)
+ ->createFirestore();
+ $collection =$firestore->collection('users');
+ $users =$collection->document('123456');
+ $user->set(['name'=>'morrislaptop','role'=>'developer']);
+ $snap=$user->snapshot();
+ echo $snap['name'];
+?>
 <head>
 	<title>Customer Registration</title>
 	<meta charset="utf-8">
