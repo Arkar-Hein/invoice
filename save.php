3fa8b1e25650a6ec0c8463dbe67c47700e7c5454
Arkar-Hein <60578548+Arkar-Hein@users.noreply.github.com>
2020-02-24 22:15:06 +0630

    Update save.php

--- save.php
+++ save.php
@@ -10,8 +10,20 @@
 </head>
 <body>
 	<?php
+use Morrislaptop\Firestore\Factory;
+use Kreait\Firebase\ServiceAccount;
+
+$serviceAccount =ServiceAccount::fromJsonFile(_DIR_'');
+$firestore=(new Factory)
+ ->withServiceAccount($serviceAccount)
+ ->createFirestore();
+ $collection =$firestore->collection('users');
+ $users =$collection->document('123456');
+ $user->set(['name'=>'morrislaptop','role'=>'developer']);
+ $snap=$user->snapshot();
+ echo $snap['name'];
 	
-		$conn=new mysqli("localhost","root","","electro");
+		
 	
 			echo "REGISTERED USER DETAILS:</br>";
 			
